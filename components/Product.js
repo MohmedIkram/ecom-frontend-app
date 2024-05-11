aaa6288a82b6a982d31d7541d877c935dcb1d905
MohmedIkram <mohmedikram123@gmail.com>
2024-05-11 20:08:57 +0530

    Version update

--- components/Product.js
+++ components/Product.js
@@ -1,3 +1,5 @@
+import Image from "next/image"
+import Link from "next/link"
 
 const products = [
     {
@@ -44,27 +46,29 @@ const products = [
   
           <div className="grid grid-cols-1 mt-6 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
             {products.map((product) => (
-              <div key={product.id} className="relative group">
+              <Link href="/product" key={product.id} className="relative group">
                 <div className="w-full overflow-hidden bg-gray-200 rounded-md group-hover:opacity-75 ">
-                  <img
+                  <Image
                     src={product.imageSrc}
                     alt={product.imageAlt}
+                    height={542}
+                    width={392}
                     className="object-cover object-center w-full h-full lg:h-full lg:w-full"
                   />
                 </div>
                 <div className="flex justify-between mt-4">
                   <div>
                     <h3 className="text-sm text-gray-700">
-                      <a href={product.href}>
+                      <span>
                         <span aria-hidden="true" className="absolute inset-0" />
                         {product.name}
-                      </a>
+                      </span>
                     </h3>
                     <p className="mt-1 text-sm text-gray-500">{product.color}</p>
                   </div>
                   <p className="text-sm font-medium text-gray-900">{product.price}</p>
                 </div>
-              </div>
+              </Link>
             ))}
           </div>
         </div>
